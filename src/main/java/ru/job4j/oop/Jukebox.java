9c6bc604722e701abb7b07b628dc9e65e9a9a59c
d.goridov <dimagoridov@yandex.ru>
2022-03-02 17:04:35 +0300

    1.3. Вызов метода с аргументами. [#166086] Метод music с параметрами

---
+++ src/main/java/ru/job4j/oop/Jukebox.java
@@ -0,0 +1,20 @@
+package ru.job4j.oop;
+
+public class Jukebox {
+    public void music(int position) {
+        if (position == 1) {
+            System.out.println("Пусть бегут неуклюже");
+        } else if (position == 2) {
+            System.out.println("Спокойной ночи");
+        } else {
+            System.out.println("Песня не найдена");
+        }
+    }
+
+    public static void main(String[] args) {
+        Jukebox session = new Jukebox();
+        session.music(1);
+        session.music(2);
+        session.music(6);
+    }
+}
