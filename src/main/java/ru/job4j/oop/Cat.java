a8a57e00310f3909fcf64cd93bd89e910e9b8909
d.goridov <dimagoridov@yandex.ru>
2022-03-02 20:57:32 +0300

    1.5. Поля объекта [#166083] Создал метод giveNick. Изменил метод show - выводится имя кота и его еда.

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -1,16 +1,29 @@
 package ru.job4j.oop;
 
 public class Cat {
+    private String food;
+    private String name;
 
-    public String sound() {
-        String voice = "may-may";
-        return voice;
+    public void show() {
+        System.out.println("Cat's name - " + this.name + ", his food - " + this.food);
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    public void giveNick(String nick) {
+        this.name = nick;
     }
 
     public static void main(String[] args) {
-        Cat peppy = new Cat();
-        Cat sparky = new Cat();
-        String say = peppy.sound();
-        System.out.println("Peppy says " + say);
+        Cat gav = new Cat();
+        gav.giveNick("Gav");
+        gav.eat("cutlet");
+        gav.show();
+        Cat black = new Cat();
+        black.giveNick("Black");
+        black.eat("fish");
+        black.show();
     }
 }
