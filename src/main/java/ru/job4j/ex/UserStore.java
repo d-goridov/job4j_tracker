5ec65622ed08be078054780614679f4c7b3f3581
d.goridov <dimagoridov@yandex.ru>
2022-03-17 19:26:48 +0300

    0.5. Иерархия исключений и множественный catch. [#219368] Реализовал методы findUser, validate. В методе main - рефакторинг кода за счет использования try-catch с множественным блоком catch.

---
+++ src/main/java/ru/job4j/ex/UserStore.java
@@ -0,0 +1,44 @@
+package ru.job4j.ex;
+
+public class UserStore {
+
+    public static User findUser(User[] users, String login) throws UserNotFoundException {
+        boolean result = false;
+        User finded = null;
+        for (User user: users) {
+            if (user.getUserName().equals(login)) {
+                result = true;
+                finded = user;
+                break;
+            }
+        }
+        if (!result) {
+            throw new UserNotFoundException("Пользователь не найден");
+        }
+        return finded;
+    }
+
+    public static boolean validate(User user) throws UserInvalidException {
+        if (!user.isValid() || user.getUserName().length() < 3) {
+            throw new UserInvalidException("Пользователь не валидный");
+        }
+        return true;
+    }
+
+    public static void main(String[] args)  {
+
+        User[] users = {
+                new User("Petr Arsentev", true)
+        };
+        try {
+            User user = findUser(users, "Petr Arsentev");
+            if (validate(user)) {
+                System.out.println("This user has an access");
+            }
+        } catch (UserInvalidException e) {
+            System.out.println(e.getMessage());
+        } catch (UserNotFoundException d) {
+            System.out.println(d.getMessage());
+        }
+    }
+}
