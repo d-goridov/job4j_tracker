99e5233833353436323e96bf138778248a6f263e
d.goridov <dimagoridov@yandex.ru>
2022-03-17 15:15:34 +0300

    0.4. Пользовательские исключения. [#219367] Добавил обработку исключения в методе поиска индекса элемента.

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,26 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int result = -1;
+        for (int i = 0; i < value.length; i++) {
+            if (value[i].equals(key)) {
+                result = i;
+            }
+        }
+        if (result == -1) {
+            throw new ElementNotFoundException("Элемент не найден");
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        try {
+            int index = indexOf(new String[]{"Ivan", "Petr", "Oleg", "Maria"}, "Olga");
+            System.out.println(index);
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
