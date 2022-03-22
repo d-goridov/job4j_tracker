6e64d81ba47c5bfabf4655cede31ca1715d57bd1
d.goridov <dimagoridov@yandex.ru>
2022-03-22 08:17:57 +0300

    1. Телефонный справочник на базе ArrayList [#41598] Доработал метод find, написал тесты

---
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.search;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+public class PhoneDictionaryTest {
+
+    @Test
+    public void whenFindByName() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(new Person("Petr", "Arsentev", "534872", "Bryansk"));
+        ArrayList<Person> peoples = phones.find("Petr");
+        assertThat(peoples.get(0).getPhone(), is("534872"));
+    }
+
+    @Test
+    public void whenNotFindByNumber() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(new Person("Petr", "Arsentev", "534872", "Bryansk"));
+        ArrayList<Person> peoples = phones.find("358");
+        assertTrue(peoples.isEmpty());
+    }
+
+}
