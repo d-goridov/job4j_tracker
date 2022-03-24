086524e1a9c73a8a0316a8b9cd3d39d9a1737bb4
d.goridov <dimagoridov@yandex.ru>
2022-03-24 19:09:13 +0300

    1. Паспорт и жители [#212656] Реализовал методы add и get Написал тест, когда будет осуществляться вставка гражданина с дублирующимся номером паспорта

---
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class PassportOfficeTest {
+
+    @Test
+    public void whenNormalAdd() {
+        Citizen citizen = new Citizen("qwe#123", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        assertThat(office.get(citizen.getPassport()), is(citizen));
+    }
+
+    @Test
+    public void whenAddDuplicate() {
+        Citizen citizen = new Citizen("qwe#123", "Petr Arsentev");
+        Citizen citizen1 = new Citizen("qwe#123", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        assertFalse(office.add(citizen1));
+    }
+}
