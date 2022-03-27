29e5fa23c50231e4e67bcc8fa17891376874cf62
d.goridov <dimagoridov@yandex.ru>
2022-03-27 09:24:13 +0300

    0. Сортировка [#10096 #280637] Рефакторинг класса ItemDescByName

--- src/main/java/ru/job4j/tracker/ItemDescByName.java
+++ src/main/java/ru/job4j/tracker/ItemDescByName.java
@@ -6,13 +6,6 @@ public class ItemDescByName implements Comparator<Item> {
 
     @Override
     public int compare(Item o1, Item o2) {
-        int result = 0;
-        if (o1.getName().compareTo(o2.getName()) > 0) {
-            result = -1;
-        }
-        if (o1.getName().compareTo(o2.getName()) < 0) {
-            result = 1;
-        }
-        return result;
+        return o2.getName().compareTo(o1.getName());
     }
 }
