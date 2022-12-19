aa7ccc7e6e7cdfa863ef4dec899e14afea71718b
d.goridov <dimagoridov@yandex.ru>
2022-12-19 19:53:20 +0300

    1. ToMany [#301848] lesson practice

--- src/main/java/ru/job4j/toone/User.java
+++ src/main/java/ru/job4j/toone/User.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Data
 @Entity
@@ -18,4 +19,7 @@ public class User {
     @ManyToOne
     @JoinColumn(name = "role_id")
     private Role role;
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "j_user_id")
+    private List<UserMessenger> messengers;
 }
